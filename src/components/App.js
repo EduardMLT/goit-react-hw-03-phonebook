33961ea288898f143d9432797edc8bc7a19a7025
EduardMLT <eddku@i.ua>
2023-09-29 10:22:49 +0300

    initial

--- src/components/App.js
+++ src/components/App.js
@@ -67,7 +67,7 @@ export class App extends Component {
   render() {
     return (
       <Wrapper>
-        <h1>Phonebook</h1>
+        <h1>Phoneboook</h1>
         <ContactForm addContact={this.addContact} />
         <h2>Contacts</h2>
         <Filter onFilter={this.filterContacts} />
